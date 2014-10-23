f54289806c455c6da82d04530e18770b693c11d2
miracode <rascatim@gmail.com>
2014-10-23 12:08:05 -0700

    made into classes and keep server open to receive multiple messages

--- echo_client.py
+++ echo_client.py
@@ -2,18 +2,26 @@
 import socket
 import sys
 
+
+class EchoClient(object):
+    def __init__(self, msg):
+        self.running = False
+        self.msg = msg
+
+    def start(self):
+        "Socket client starting\n"
+        client_socket = socket.socket(
+            socket.AF_INET,
+            socket.SOCK_STREAM,
+            socket.IPPROTO_IP)
+        client_socket.connect(('127.0.0.1', 50000))
+
+        client_socket.sendall(self.msg)
+        client_socket.shutdown(socket.SHUT_WR)
+
+        client_socket.close()
+
+
 if __name__ == '__main__':
-
-    msg = sys.argv[1]
-
-    client_socket = socket.socket(
-        socket.AF_INET,
-        socket.SOCK_STREAM,
-        socket.IPPROTO_IP)
-
-    client_socket.connect(('127.0.0.1', 50000))
-
-    client_socket.sendall(msg)
-    client_socket.shutdown(socket.SHUT_WR)
-
-    client_socket.close()
+    client = EchoClient(sys.argv[1])
+    client.start()
--- echo_server.py
+++ echo_server.py
@@ -1,17 +1,32 @@
 #!/usr/bin/env python
 import socket
 
+
+class EchoServer(object):
+    def __init__(self):
+        self.running = False
+
+    def start(self):
+        self.running = True
+        print "Socket server starting\n"
+        server_socket = socket.socket(
+            socket.AF_INET,
+            socket.SOCK_STREAM,
+            socket.IPPROTO_IP)
+        server_socket.bind(('127.0.0.1', 50000))
+        server_socket.listen(1)
+        while self.running:
+            conn, addr = server_socket.accept()
+            message = conn.recv(32)
+            print message
+            conn.close()
+        server_socket.close()
+        print "Socket server dying\n"
+
+    def stop(self):
+        self.running = False
+
+
 if __name__ == '__main__':
-    server_socket = socket.socket(
-        socket.AF_INET,
-        socket.SOCK_STREAM,
-        socket.IPPROTO_IP)
-    server_socket.bind(('127.0.0.1', 50000))
-    server_socket.listen(1)
-    conn, addr = server_socket.accept()
-
-    message = conn.recv(32)
-    print message
-
-    conn.close()
-    server_socket.close()
+    server = EchoServer()
+    server.start()
