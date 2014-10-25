6ce3e1bbfbb15e6339cd3c071878cf29437e4b6c
miracode <rascatim@gmail.com>
2014-10-25 14:11:22 -0700

    returns error codes

--- http_server.py
+++ http_server.py
@@ -1,8 +1,11 @@
 #!/usr/bin/env python
+# -*- coding: utf-8 -*-
 import socket
 from threading import Thread
 import Queue
 import time
+#import urllib2
+#import BaseHTTPServer
 
 
 class HttpServer(Thread):
@@ -28,14 +31,8 @@ class HttpServer(Thread):
         http_socket.listen(1)
         while self.running:
             print "Socket server waiting to accept new connection\n"
-            try:
-                self.conn_queue.put(http_socket.accept())
-                conn, addr = http_socket.accept()
-            except KeyboardInterrupt:
-                "exit"
-                http_socket.shutdown()
-                http_socket.close()
-                self.running = False
+            conn, addr = http_socket.accept()
+            self.conn_queue.put((conn, addr))
 
         print "Socket server shutting down\n"
         http_socket.shutdown()
@@ -77,6 +74,28 @@ class ProtocolWorker(Thread):
                     conn.shutdown(socket.SHUT_RDWR)
                     conn.close()
 
+    def receive_message(self, conn):
+        message = ""
+        while not message.endswith("\r\n"):
+            received = conn.recv(1024)
+            message += received
+        return message
+
+    def parse_message(self, message):
+        parsed = message.split()
+        method = parsed[0]
+        protocol = parsed[2]
+        return method, protocol
+
+    def return_response(self, method, protocol):
+        if method == 'GET':
+            if protocol == 'HTTP/1.1':
+                return '%s 200 OK\r\n\r\n' % protocol
+            else:
+                return '%s 505 HTTP Version Not Supported\r\n\r\n' % protocol
+        else:
+            return '%s 405 Method Not Allowed\r\n\r\n' % method
+
 
 class HttpProtocolWorker(ProtocolWorker):
     def __init__(self, conn_queue):
@@ -84,17 +103,12 @@ class HttpProtocolWorker(ProtocolWorker):
 
     def execute_protocol_logic(self, conn, client_addr):
         print "HttpProtocolWorker handling new HTTP connection"
-        http_req = ""
-        data = bytearray(1024)
-        while not http_req.endswith("\r\n\r\n"):
-            print "HttpProtocolWorker reading data from client"
-            read_bytes = conn.recv_into(data, 1024)
-            if read_bytes == 0:
-                print "HttpProtocolWorker connection lost"
-                return None
-            http_req += data[:read_bytes]
-        print "HttpProtocolWorker got all data"
-        conn.sendall("HTTP/1.1 200 OK\r\nYou said: " + http_req + "\r\n\r\n")
+        message = self.receive_message(conn)
+        print "HttpProtocolWorker got all data:"
+        method, protocol = self.parse_message(message)
+        response = self.return_response(method, protocol)
+        print "response:" + response  # cut off return lines
+        conn.sendall(response)
         print "HttpProtocolWorker done"
 
 
@@ -102,5 +116,10 @@ if __name__ == '__main__':
     global_conn_queue = Queue.Queue()
     worker = HttpProtocolWorker(global_conn_queue)
     server = HttpServer(global_conn_queue)
-    worker.start()
-    server.start()
+    try:
+        worker.start()
+        server.start()
+    except KeyboardInterrupt:
+        server.running = False
+        server.close()
+        print "Exiting"
